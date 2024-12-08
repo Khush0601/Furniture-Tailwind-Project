b12764f315fd063d3f5f0ea8fc7d1e7265307e67
khushboo kumari <ksingh200601@gmail.com>
2024-12-09 01:07:07 +0530

    modified app.js an implemented theme

--- src/App.js
+++ src/App.js
@@ -1,71 +1,83 @@
-import React from 'react'
+import React, { useState } from 'react'
 import icon from '../src/Assets/icon.png'
 import AboutImg from '../src/Assets/AboutImg.jpg'
 import data from '../src/Data/data'
-import Card from './component/card'
+import Card from './component/card' 
+
+import './App.css'
 const App = () => {
+  const [isLight,setIsLight]=useState(true)
+
   return (
-    <div className='w-screen h-screen bg-[black]'>
-    <nav className='w-full h-14 bg-slate-900 flex justify-center items-center text-[white] shadow-md '>
-      <div className=' w-4/5 flex justify-between '>
-        <div className='flex  items-center '>
-          <div >
-            <img src={icon} alt='home-icon' className='w-4 h-4'/>
-          </div>
-          <div className='text-lg px-1'>Furnitura</div>
-        </div>
-        <div className='text-lg'>
-          <div className=' xs:hidden'>Menu</div>
-          <div className='hidden xs:flex gap-x-2.5'>
-          <div>Home</div>
-          <div>About</div>
-          <div>Services</div>
-          <div>Contact</div>
-          </div>
-        </div>
-      </div>
-    </nav>
-    <section className='w-full bg-black-200 flex justify-center items-center text-[white]'>
-       <div className='w-full  flex justify-center items-center flex-col px-5 my-40'>
+   <>
+   <div className='fixed right-2 bottom-2'>
+    <label htmlFor='themeCheckBox'>{isLight?'light':"dark"}</label>
+    <input id='themeCheckBox' type='checkbox' checked={isLight} onChange={()=>setIsLight((p)=>!p)}/>
+   </div>
+     <div className={`w-screen  ${isLight?'light':'dark'} tapp-bg`}>
+   <nav className='px-5 xs:px-10 py-3 bg-slate-200 grid grid-cols-12 gap-2'>
+     <div className='col-span-6 xs:col-span-4  flex items-center'>
+     <img src={icon} alt='home-icon' className='w-6 h-6'/>
+     <div className='text-lg px-1'>Furnitura</div>
+     </div>
+     <div className='col-span-6 xs:col-span-8  flex items-center justify-end '>
+     <div className='xs:hidden'>Menu</div>
+          <div className='hidden xs:flex '>
+          <div className='mx-2'>Home</div>
+          <div className='mx-2'>About</div>
+          <div className='mx-2'>Services</div>
+          <div className='ml-2'>Contact</div>
+     </div>
+     </div>
+   </nav>
+   <section className='px-5  xs:px-10 '>
+   <div className='h-[700px] py-10 home-banner flex flex-col justify-center items-center'>
         <div className='text-3xl text-center'>Transform Your Space With Our </div>
         <div className='text-3xl text-center'>Premium Furnitures</div>
         <div className='text-center'>
         Furniture refers to objects intended to support various human activities such 
         as seating (e.g., stools, chairs, and sofas), eating (tables), storing items, working, and sleeping 
         </div>
-       </div>
-    </section>
-    <section className='w-full  bg-slate-400 flex justify-center py-20'>
-      <div className=' w-4/5 grid grid-cols-1 md:grid-cols-2 items-center'>
-        <div className='w-full flex justify-center p-1.5 md:flex items-center'>
-          <img src={AboutImg} alt="aboutImg" className='w-96 h-96 md:w-96 h-96 object-cover'/>
+        <div className='flex gap-4 my-6'>
+          <button className='px-8 py-2 explore-button rounded-lg'>Shop Now</button>
+          <button className='px-8 py-2  explore-button rounded-lg'>Explore</button>
         </div>
-        <div className=' w-full flex flex-col justify-center p-10 md:align-center'>
-            <div className='text-4xl text-center md:text-left'>About Us</div>
-            <div className='my-4 text-center md:text-left'>
+   </div>
+
+   <div className='py-10 px-2 my-4 grid grid-cols-1 md:grid-cols-2  tapp-bg gap-2 '>
+    <div className='  flex justify-center items-center '>
+    <img src={AboutImg} alt="aboutImg" className=' h-96'/>
+    </div>
+    <div className='  flex flex-col justify-center '>
+    <div className='text-4xl text-center md:text-left tapp-text'>About Us</div>
+            <div className='my-4 text-center md:text-left tapp-text '>
             Furniture is a human-made object that serves a variety of purposes,
              including seating, eating, sleeping, and working. It can be used to hold 
              items at a convenient height, or to store things. 
             </div>
-            <div className='text-center md:text-left'>
+            <div className='text-center md:text-left tapp-text'>
         Furniture can be a product of design and can be considered a 
         form of decorative art. The functional and decorative aspects of furniture
          have been emphasized more or less throughout history according to economics and fashion.
         </div>
-        </div>
-     </div>
-    </section>
-    <section className='w-full   flex justify-center items-center'>
-       <div className='w-4/5  flex flex-wrap  justify-center items-center'>
+    </div>
+   </div>
+   <div className='py-10 px-2 my-4   tapp-bg flex flex-wrap justify-center'>
+  
           {
             data.map((el,i)=>{
               return <Card key={i} el={el} />
             })
           }
-       </div>
-    </section>
+     
+
+   </div>
+   </section>
+   
+   
   
     </div>
+   </>
   )
 }
 
--- src/component/card.js
+++ src/component/card.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 const Card = ({el}) => {
   return (
-    <div className='w-80 h-96  m-4 flex flex-col shadow-md rounded-xl '>
+    <div className='w-80 h-96  m-4 flex flex-col border-[1px] shadow-md rounded-xl tapp-border'>
       <img src={el.image} alt={el.name} className='w-80 h-64'/>
       <div className='px-2.5 py-2'>
-      <div className='font-semibold'>{el.name}</div>
-      <div>{el.description}</div>
-      <div>{el.price}</div>
+      <div className='font-semibold tapp-text'>{el.name}</div>
+      <div className='tapp-text'>{el.description}</div>
+      <div className='tapp-text'>{el.price}</div>
       </div>
     </div>
   )

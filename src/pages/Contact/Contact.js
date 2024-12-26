bc46dd935394d727714391fb3bf8b16d4c1935f0
khushboo kumari <ksingh200601@gmail.com>
2024-12-26 11:22:34 +0530

    contact Page made

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import Nav from '../../component/Nav/Nav'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div>
+      <Nav/>
+      <section className='h-[700px] contact-banner flex justify-center items-center md:justify-end'>
+       <div className='h-[400px] w-[400px] bg-slate-50 md:mr-24 p-5'>
+        <div className='text-2xl'>Contact Us</div>
+        <form className='flex flex-col py-8'>
+          <input placeholder='enter Name' type='text' className='border-[1px] border-solid rounded-xl p-4 border-black mb-5' />
+          <input placeholder='enter Email' type='text' className='border-[1px] border-solid rounded-xl p-4 border-black mb-5'/>
+          <input placeholder='enter phone' type='text' className='border-[1px] border-solid rounded-xl p-4 border-black mb-5'/>
+          <button className='text-start w-20 bg-blue-800 text-white px-4 py-2 rounded-xl text-center'>Submit</button>
+        </form>
+       </div>
+      </section>
+    </div>
   )
 }
 

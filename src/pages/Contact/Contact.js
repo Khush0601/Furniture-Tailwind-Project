88d05476606182b91992681ad94faecd5b3814bc
khushboo kumari <ksingh200601@gmail.com>
2024-12-26 21:14:12 +0530

    modified

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -6,13 +6,13 @@ const Contact = () => {
     <div>
       <Nav/>
       <section className='h-[700px] contact-banner flex justify-center items-center md:justify-end'>
-       <div className='h-[400px] w-[400px] bg-slate-50 md:mr-24 p-5'>
+       <div className=' w-[400px] border-2 border-green-700 border-solid rounded-xl m-4 md:mr-24 p-5'>
         <div className='text-2xl'>Contact Us</div>
-        <form className='flex flex-col py-8'>
+        <form className='flex flex-col py-4'>
           <input placeholder='enter Name' type='text' className='border-[1px] border-solid rounded-xl p-4 border-black mb-5' />
           <input placeholder='enter Email' type='text' className='border-[1px] border-solid rounded-xl p-4 border-black mb-5'/>
           <input placeholder='enter phone' type='text' className='border-[1px] border-solid rounded-xl p-4 border-black mb-5'/>
-          <button className='text-start w-20 bg-blue-800 text-white px-4 py-2 rounded-xl text-center'>Submit</button>
+          <button className=' w-20 bg-blue-800 text-white px-4 py-2 rounded-xl text-center'>Submit</button>
         </form>
        </div>
       </section>

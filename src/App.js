ba6a743db3c175894cb3b9070021e8c1da25959d
khushboo kumari <ksingh200601@gmail.com>
2025-01-09 23:40:38 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -9,11 +9,12 @@ const App = () => {
    <>
    <div className='fixed right-2 bottom-2'>
     <label htmlFor='themeCheckBox'>{isLight?'light':"dark"}</label>
-    <input id='themeCheckBox' type='checkbox' checked={isLight} onChange={()=>setIsLight((p)=>!p)}/>
+    <input id='themeCheckBox' type ='checkbox' checked={isLight} onChange={()=>setIsLight((p)=>!p)}/>
    </div>
     
    <div className={`${isLight?'light':'dark'} tapp-bg`}>
     <AppRoutes/>
+
     </div>
    </>
   )
